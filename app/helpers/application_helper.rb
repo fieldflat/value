c05be7c17148651dfe7309f0241a47825dd83657
Tomonori HIRATA <hirata@sqlab.jp>
2019-12-19 12:09:01 +0900

    update: Home画面をパーシャル化＆ヘッダー・フッターをパーシャル化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページタイトルを設定する
+  def full_title(page_title = '')
+    base_title = "Value"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
