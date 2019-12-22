83fabe9c668e82dccd94c652b7965007672029c9
Tomonori HIRATA <hirata@sqlab.jp>
2019-12-22 14:34:11 +0900

    update: ログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
@@ -7,5 +8,8 @@ Rails.application.routes.draw do
   get  'static_pages/creator_info'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
