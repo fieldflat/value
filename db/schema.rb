de2a9a8afc4930bcae277ff95bcdb8a8d5249e53
Tomonori HIRATA <hirata@sqlab.jp>
2019-12-22 16:41:36 +0900

    update: navbarの改修＆remember_meの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_051434) do
+ActiveRecord::Schema.define(version: 2019_12_22_070805) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_051434) do
     t.datetime "updated_at", null: false
     t.string "university"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
