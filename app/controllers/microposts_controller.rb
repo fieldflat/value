f73d7885f58715ef28420bf316ba3722d6f21e86
Tomonori HIRATA <hirata@sqlab.jp>
2019-12-25 00:31:41 +0900

    update: 出品できるようにする

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,53 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :show, :edit, :create, :destroy, :update]
+  before_action :correct_user, only: [:edit, :destroy, :update]
+
+  def new
+    @micropost = current_user.microposts.build
+  end
+
+  def show
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def create
+    @micropost = current_user.microposts.build(microposts_params)
+    if @micropost.save
+      flash[:success] = "出品しました！"
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost Deleted!!"
+    redirect_to current_user
+  end
+
+  def update
+    if @micropost.update_attributes(microposts_params)
+      flash[:success] = "編集しました！"
+      redirect_to @micropost
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def microposts_params
+      params.require(:micropost).permit(:title, :content, :price, :picture)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,18 +51,14 @@ class UsersController < ApplicationController
                                    :university, :grade)
     end
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうかを確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
